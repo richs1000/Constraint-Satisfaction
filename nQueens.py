3d5b4507201b2b71a0855b8825e05daa2627c5de
Rich Simpson <richs1000@mac.com>
2014-11-27 10:01:11 -0500

    Separates n-queens constraint into two constraints
    Created a column constraint and a diagonal constraint to illustrate
    that you can have more than one constraint between features

--- nQueens.py
+++ nQueens.py
@@ -3,7 +3,7 @@ __author__ = 'rsimpson'
 from constraintSatisfaction import *
 
 # This value is used to determine the size of the board when doing an n-queens problem
-GRIDSIZE = 5
+GRIDSIZE = 15
 
 # This is a list of lists - where each sub-list contains the indices that are in the
 # same column.
@@ -40,7 +40,49 @@ def createNQueensGlobals():
         NQUEENS_DIAGONALS.append(range(index * GRIDSIZE, GRIDSIZE * GRIDSIZE, GRIDSIZE + 1))
 
 
-class CSPConstraintQueens(CSPConstraint):
+# class CSPConstraintQueens(CSPConstraint):
+#     def __init__(self, ftrTail, strConstraint, ftrHead):
+#         """
+#         This function sets up the values for an N-Queens constraint. Right now, each constraint object
+#         stores its own column list and diagonal list, which is inefficient. I should really change this
+#         so I only store it once.
+#         """
+#         # access the global variables
+#         global GRIDSIZE
+#         global NQUEENS_DIAGONALS
+#         global NQUEENS_COLUMNS
+#         # call the parent constructor
+#         CSPConstraint.__init__(self, ftrTail, strConstraint, ftrHead)
+#         # define the grid size (assume it's a square) based on the number of features
+#         # (one feature for each queen means one feature for each row)
+#         self.gridSize = GRIDSIZE
+#         # store a pointer to the list of column lists
+#         self.columns = NQUEENS_COLUMNS
+#         # store a pointer to the list of diagonals
+#         self.diagonals = NQUEENS_DIAGONALS
+#
+#     def satisfied(self, tailValue, headValue):
+#         """
+#         returns true if constraint is satisfied and false if it is not
+#         """
+#         # if the head value or tail value is unassigned then we're done
+#         if tailValue == "none" or headValue == "none":
+#             return True
+#         # loop through the list of column lists
+#         for columnList in self.columns:
+#             # if both features are assigned and in the same column then return false
+#             if ((int(tailValue) in columnList) and (int(headValue) in columnList)):
+#                 return False
+#         # loop through the list of diagonal lists
+#         for diagonalList in self.diagonals:
+#             # if both features are assigned and in the same diagonal then return false
+#             if ((int(tailValue) in diagonalList) and (int(headValue) in diagonalList)):
+#                 return False
+#         # otherwise, all constraints are satisfied so return true
+#         return True
+
+
+class CSPConstraintSameColumn(CSPConstraint):
     def __init__(self, ftrTail, strConstraint, ftrHead):
         """
         This function sets up the values for an N-Queens constraint. Right now, each constraint object
@@ -58,8 +100,6 @@ class CSPConstraintQueens(CSPConstraint):
         self.gridSize = GRIDSIZE
         # store a pointer to the list of column lists
         self.columns = NQUEENS_COLUMNS
-        # store a pointer to the list of diagonals
-        self.diagonals = NQUEENS_DIAGONALS
 
     def satisfied(self, tailValue, headValue):
         """
@@ -73,6 +113,35 @@ class CSPConstraintQueens(CSPConstraint):
             # if both features are assigned and in the same column then return false
             if ((int(tailValue) in columnList) and (int(headValue) in columnList)):
                 return False
+        # otherwise, all constraints are satisfied so return true
+        return True
+
+
+class CSPConstraintSameDiagonal(CSPConstraint):
+    def __init__(self, ftrTail, strConstraint, ftrHead):
+        """
+        This function sets up the values for an N-Queens constraint. Right now, each constraint object
+        stores its own column list and diagonal list, which is inefficient. I should really change this
+        so I only store it once.
+        """
+        # access the global variables
+        global GRIDSIZE
+        global NQUEENS_DIAGONALS
+        # call the parent constructor
+        CSPConstraint.__init__(self, ftrTail, strConstraint, ftrHead)
+        # define the grid size (assume it's a square) based on the number of features
+        # (one feature for each queen means one feature for each row)
+        self.gridSize = GRIDSIZE
+        # store a pointer to the list of diagonals
+        self.diagonals = NQUEENS_DIAGONALS
+
+    def satisfied(self, tailValue, headValue):
+        """
+        returns true if constraint is satisfied and false if it is not
+        """
+        # if the head value or tail value is unassigned then we're done
+        if tailValue == "none" or headValue == "none":
+            return True
         # loop through the list of diagonal lists
         for diagonalList in self.diagonals:
             # if both features are assigned and in the same diagonal then return false
@@ -116,22 +185,29 @@ def NQueens():
     # add constraints
     for q1 in range(0, GRIDSIZE):
         for q2 in range(q1+1, GRIDSIZE):
-#            cspGraph.addConstraint('Q'+str(q1), 'Queens', 'Q'+str(q2))
             ftrTail = 'Q' + str(q1)
             ftrHead = 'Q' + str(q2)
             strConstraint = 'Queens'
-            # create a new constraint object from tail to head
-            newConstraint = CSPConstraintQueens(cspGraph.getFeature(ftrTail), strConstraint, cspGraph.getFeature(ftrHead))
+            # create a new column constraint object from tail to head
+            newConstraint = CSPConstraintSameColumn(cspGraph.getFeature(ftrTail), strConstraint, cspGraph.getFeature(ftrHead))
             # put the new constraint in the graph's list of constraints
             cspGraph.constraints.append(newConstraint)
-            # create a new constraint object from head to tail
-            newConstraint = CSPConstraintQueens(cspGraph.getFeature(ftrHead), strConstraint, cspGraph.getFeature(ftrTail))
+            # create a new column constraint object from head to tail
+            newConstraint = CSPConstraintSameColumn(cspGraph.getFeature(ftrHead), strConstraint, cspGraph.getFeature(ftrTail))
+            # put the new constraint in the graph's list of constraints
+            cspGraph.constraints.append(newConstraint)
+            # create a new diagonal constraint object from tail to head
+            newConstraint = CSPConstraintSameDiagonal(cspGraph.getFeature(ftrTail), strConstraint, cspGraph.getFeature(ftrHead))
+            # put the new constraint in the graph's list of constraints
+            cspGraph.constraints.append(newConstraint)
+            # create a new diagonal constraint object from head to tail
+            newConstraint = CSPConstraintSameDiagonal(cspGraph.getFeature(ftrHead), strConstraint, cspGraph.getFeature(ftrTail))
             # put the new constraint in the graph's list of constraints
             cspGraph.constraints.append(newConstraint)
 
 
     # call backtracking search
-    #backtrackingSearch(cspGraph, 0)
-    hillClimbingSearch(cspGraph)
+    backtrackingSearch(cspGraph, 0)
+    #hillClimbingSearch(cspGraph)
 
 NQueens()
